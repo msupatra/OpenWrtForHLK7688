8c72c533f1a91c12495b97d3d257c6451b5c9ee0
msupatra <48304088+msupatra@users.noreply.github.com>
2026-02-26 09:05:02 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,11 @@
-# ลบบรรทัดที่ซ้ำซ้อนออกและใช้เพียงชุดนี้
+# 1. เปิดสถานะ SPI เป็น okay
 sed -i 's/status = "disabled";/status = "okay";/g' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
 
-sed -i '/spi0 {/,/};/ s/status = "okay";/status = "okay";\n\tspidev@1 {\n\t\tcompatible = "linux,spidev";\n\t\treg = <1>;\n\t\tspi-max-frequency = <1000000>;\n\t};/' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
+# 2. แทรก spidev@1 เข้าไปหลังบรรทัดที่มี status = "okay"; ภายในโหนด spi0 
+# โดยใช้คำสั่งที่ระมัดระวังเรื่องลำดับ (ใช้ a เพื่อ append ต่อท้าย)
+sed -i '/&spi0 {/,/};/ { /status = "okay";/ a \
+	spidev@1 {\
+		compatible = "linux,spidev";\
+		reg = <1>;\
+		spi-max-frequency = <1000000>;\
+	};' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
