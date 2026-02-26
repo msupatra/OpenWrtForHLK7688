6028c488414fd300d7e44170d3465142113e3101
msupatra <48304088+msupatra@users.noreply.github.com>
2026-02-26 11:20:15 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,11 +1,48 @@
-# 1. เปิดสถานะ SPI เป็น okay
-sed -i 's/status = "disabled";/status = "okay";/g' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
+#!/bin/bash
 
-# 2. แทรก spidev@1 เข้าไปหลังบรรทัดที่มี status = "okay"; ภายในโหนด spi0 
-# โดยใช้คำสั่งที่ระมัดระวังเรื่องลำดับ (ใช้ a เพื่อ append ต่อท้าย)
-sed -i '/&spi0 {/,/};/ { /status = "okay";/ a \
-	spidev@1 {\
-		compatible = "linux,spidev";\
-		reg = <1>;\
-		spi-max-frequency = <1000000>;\
-	};' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
+# สร้างไฟล์ DTS ใหม่ทับตัวเดิม เพื่อเปิด SPI และแก้ปัญหาโครงสร้างไฟล์
+cat <<EOF > target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
+/dts-v1/;
+
+#include "mt7628an.dtsi"
+#include <dt-bindings/gpio/gpio.h>
+#include <dt-bindings/input/input.h>
+
+/ {
+	compatible = "hilink,hlk-7688a", "mediatek,mt7628an-soc";
+	model = "HiLink HLK-7688A";
+
+	aliases {
+		label-mac-device = &ethernet;
+	};
+
+	chosen {
+		bootargs = "console=ttyS0,115200";
+	};
+};
+
+&state_default {
+	gpio {
+		groups = "p0led", "p1led", "p2led", "p3led", "p4led", "wdt", "wled_an";
+		function = "gpio";
+	};
+};
+
+&spi0 {
+	status = "okay";
+
+	spidev@1 {
+		compatible = "linux,spidev";
+		reg = <1>;
+		spi-max-frequency = <1000000>;
+	};
+};
+
+&ethernet {
+	mtd-mac-address = <&factory 0x28>;
+};
+
+&wmac {
+	status = "okay";
+};
+EOF
