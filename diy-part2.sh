69e50e91b7def2065bfbc443786bff5b12f61618
msupatra <48304088+msupatra@users.noreply.github.com>
2026-02-28 05:17:05 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# 1. ค้นหาไฟล์ DTS ของ HLK-7688A และเปิดใช้งาน SPI0
-# โดยการเปลี่ยนสถานะจาก disabled เป็น okay
+# 1. เปิดใช้งาน SPI0 โดยเปลี่ยนสถานะจาก disabled เป็น okay
 sed -i 's/status = "disabled";/status = "okay";/g' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
 
-# 2. แทรกโครงสร้าง spidev@1 เข้าไปก่อนเครื่องหมายปีกกาปิดตัวสุดท้ายของไฟล์
-# วิธีนี้จะปลอดภัยที่สุดเพราะไม่ไปขวางลำดับ Property เดิมที่ทำให้เกิด Error "Properties must precede subnodes"
-sed -i '$i \
-&spi0 {\
-	spidev@1 {\
-		compatible = "linux,spidev";\
-		reg = <1>;\
-		spi-max-frequency = <1000000>;\
-	};\
-};' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
+# 2. ใช้คำสั่ง printf เขียนโค้ด SPI ต่อท้ายไฟล์โดยตรง (วิธีนี้จะไม่กวนโครงสร้างเดิมและไม่มี Syntax Error)
+printf '\n&spi0 {\n\tspidev@1 {\n\t\tcompatible = "linux,spidev";\n\t\treg = <1>;\n\t\tspi-max-frequency = <1000000>;\n\t};\n};\n' >> target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
