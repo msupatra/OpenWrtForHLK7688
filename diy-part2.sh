00e6f4b784634480e3d77067c826fd0341ff8a5c
msupatra <48304088+msupatra@users.noreply.github.com>
2026-02-27 15:11:48 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# 1. เปลี่ยนสถานะ spi0 จาก disabled เป็น okay
+# 1. ค้นหาและเปลี่ยนสถานะ spi0 จาก disabled เป็น okay
 sed -i 's/status = "disabled";/status = "okay";/g' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
 
-# 2. แก้ปัญหา Properties must precede subnodes โดยการแทรก spidev ไว้หลังสุดของก้อน spi0
-# เราจะหาบรรทัด 'status = "okay";' ที่อยู่ภายในโหนด spi0 แล้วแทนที่ด้วยตัวมันเองตามด้วย subnode
-sed -i '/&spi0 {/,/};/ s/status = "okay";/status = "okay";\n\n\tspidev@1 {\n\t\tcompatible = "linux,spidev";\n\t\treg = <1>;\n\t\tspi-max-frequency = <1000000>;\n\t};/' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
+# 2. ใช้เทคนิคแทรก spidev@1 ไว้ก่อนเครื่องหมายปีกกาปิดตัวสุดท้ายของก้อน &spi0
+# วิธีนี้จะรับประกันว่า spidev จะอยู่หลัง Property ทุกตัวแน่นอน
+sed -i '/&spi0 {/,/};/ { /};/ i \
+\	spidev@1 {\
+\		compatible = "linux,spidev";\
+\		reg = <1>;\
+\		spi-max-frequency = <1000000>;\
+\	};' target/linux/ramips/dts/mt7628an_hilink_hlk-7688a.dts
